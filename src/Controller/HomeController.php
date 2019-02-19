c0315ced271b3c9bcebeff516dffaf433494f621
LTFabien <letrungfabien@gmail.com>
2019-02-19 19:28:06 +0100

    Initialization of first bootstrap

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fabien
+ * Date: 19/02/2019
+ * Time: 18:15
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @Route("/")
+     */
+    public function homepage(): Response
+    {
+        return new Response($this->twig->render('pages/home.html.twig'));
+    }
+}
