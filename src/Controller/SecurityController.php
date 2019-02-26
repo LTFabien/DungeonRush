d7359e791d745ea3a17e90aeac8711deab442252
Nadir Khattab <khattab.nadir@gmail.com>
2019-02-26 20:23:11 +0100

    Encode passwords

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 class SecurityController extends AbstractController
@@ -16,7 +17,7 @@ class SecurityController extends AbstractController
     /**
      * @Route("/inscription", name="security_registration")
      */
-    public function registration(Request $request, ObjectManager $manager){
+    public function registration(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder){
         $user=new User();
 
         $form= $this->createForm(RegistrationType::class, $user);
@@ -24,6 +25,8 @@ class SecurityController extends AbstractController
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
+            $hash=$encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hash);
             $manager->persist($user);
             $manager->flush();
         }
