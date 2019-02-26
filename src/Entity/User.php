03f2ade4c995c15426a44ec70105f38ff5619cf3
Nadir Khattab <khattab.nadir@gmail.com>
2019-02-26 20:45:20 +0100

    UniqueEntity User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,11 +3,16 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(
+ *     fields={"email"},
+ *     message="L'email que vous avez saisi est déjà utilisé",
+ * )
  */
 class User implements UserInterface
 {
@@ -31,6 +36,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Email()
      */
     private $email;
 
