69fd5dad279b26bdf481c187f01590a314721163
Kerryo <arnaudkerry@gmail.com>
2019-03-12 21:26:29 +0100

    Signup & Login Forms (bug on Login Form #InvalidCredential)

--- src/Controller/SignupController.php
+++ src/Controller/SignupController.php
@@ -9,6 +9,9 @@
 namespace App\Controller;
 
 
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,8 +22,24 @@ class SignupController extends AbstractController
     /**
      * @Route("/signup", name="signup")
      */
-    public function inscription(): Response
+    public function inscription(Request $request)
     {
-        return $this->render('pages/signup.html.twig');
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($user);
+            $em->flush();
+
+        }
+
+        return $this->render('pages/signup.html.twig', array(
+            'form' => $form->createView(),
+    ));
     }
 }
